329cef9f08fc86210da060d6ec833704bf507ad7
AZch <antonzhuikov@gmail.com>
2020-11-15 20:26:29 +0700

    add availability to inject assemblers

--- src/main/java/com/wordscreators/easyCook/recipe/assembler/IngredientCollectionModelAssembler.java
+++ src/main/java/com/wordscreators/easyCook/recipe/assembler/IngredientCollectionModelAssembler.java
@@ -5,12 +5,14 @@ import com.wordscreators.easyCook.recipe.model.Ingredient;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class IngredientCollectionModelAssembler implements RepresentationModelAssembler<List<EntityModel<Ingredient>>, CollectionModel<EntityModel<Ingredient>>> {
     @Override
     public CollectionModel<EntityModel<Ingredient>> toModel(List<EntityModel<Ingredient>> entities) {
--- src/main/java/com/wordscreators/easyCook/recipe/assembler/IngredientModelAssembler.java
+++ src/main/java/com/wordscreators/easyCook/recipe/assembler/IngredientModelAssembler.java
@@ -4,10 +4,12 @@ import com.wordscreators.easyCook.recipe.controller.IngredientController;
 import com.wordscreators.easyCook.recipe.model.Ingredient;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class IngredientModelAssembler implements RepresentationModelAssembler<Ingredient, EntityModel<Ingredient>> {
     @Override
     public EntityModel<Ingredient> toModel(Ingredient entity) {
